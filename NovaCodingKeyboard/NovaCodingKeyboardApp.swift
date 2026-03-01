fec1486e42abd3603914cb02945e0d431765ee5a
GaoShang <styleshang@outlook.com>
2026-03-01 08:48:44 -0800

    Initial Commit

---
+++ NovaCodingKeyboard/NovaCodingKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaCodingKeyboardApp.swift
+//  NovaCodingKeyboard
+//
+//  Created by Nova on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct NovaCodingKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
